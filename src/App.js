d52b5957920502618cf13a0b92e75df6b4eade73
Arasu Ramanan <arasuaudi@gmail.com>
2026-08-12 00:28:44 +0530

    Fix protected route rendering

--- src/App.js
+++ src/App.js
@@ -1,29 +1,78 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Login from './Components/Login';
-import Signup from './Components/Signup';
-import Header from './Components/Header/Header';
-import DetailsList from './pages/DetailList/DetailList';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import Header from "./Components/Header/Header";
+
+import DetailsList from "./pages/DetailList/DetailList";
 import EnteredDocuments from "./pages/EnteredDocuments/EnteredDocuments";
-import AuditTrail from './pages/AuditTrail/AuditTrail';
+import AuditTrail from "./pages/AuditTrail/AuditTrail";
+
 import ProtectedRoute from "./Components/ProtectedRoute";
+
 import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Toaster position="top-right" reverseOrder={false} />
-         <Routes>
-        <Route element={<ProtectedRoute />}>
-          <Route path="/list" element={<div><Header /><DetailsList /></div>} />
+
+        <Toaster
+          position="top-right"
+          reverseOrder={false}
+        />
+
+        <Routes>
+
+          {/* Public Routes */}
           <Route path="/" element={<Login />} />
+
           <Route path="/signup" element={<Signup />} />
-          <Route path="/entered-documents" element={<div><Header /><EnteredDocuments /></div>}/>
-          <Route path="/audit-trail" element={<div><Header /><AuditTrail /></div>}/>
-        </Route>
+
+          {/* Protected Routes */}
+          <Route element={<ProtectedRoute />}>
+
+            <Route
+              path="/list"
+              element={
+                <div>
+                  <Header />
+                  <DetailsList />
+                </div>
+              }
+            />
+
+            <Route
+              path="/entered-documents"
+              element={
+                <div>
+                  <Header />
+                  <EnteredDocuments />
+                </div>
+              }
+            />
+
+            <Route
+              path="/audit-trail"
+              element={
+                <div>
+                  <Header />
+                  <AuditTrail />
+                </div>
+              }
+            />
+
+          </Route>
+
         </Routes>
+
       </div>
     </Router>
   );
